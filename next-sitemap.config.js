035cf4eb9bc56aa6a8d6a1816c9fac5d58f5e3d9
jackoregankenny <42699197+jackoregankenny@users.noreply.github.com>
2024-08-01 15:00:01 +0100

    chore: Update next-sitemap.config.js and public/_headers

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.SITE_URL || 'https://jackoregankenny.com',
-    generateRobotsTxt: true,
-    // other options...
-  }
+  siteUrl: process.env.SITE_URL || 'https://jackoregankenny.com',
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
+  },
+  exclude: ['/server-sitemap.xml'], // exclude server-side sitemap if you have one
+  generateIndexSitemap: false, // set to true if you have more than 50,000 urls
+}
