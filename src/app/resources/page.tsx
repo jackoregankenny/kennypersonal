2031542050ea272769657e369a35e2f76e9f2d20
jackoregankenny <42699197+jackoregankenny@users.noreply.github.com>
2024-08-12 22:28:05 +0100

    Update page titles and headings for consistency

--- src/app/resources/page.tsx
+++ src/app/resources/page.tsx
@@ -7,7 +7,7 @@ export const revalidate = 3600; // Revalidate every hour
 export default async function ResourcesPage() {
   const resources = await getResources();
 
-  const introText = "I used to have a list of things, mainly tools I reccomended to early stage companies or patch teams, usually to speed up things or as effective low cost alternatives to somethig they already used. I lost that file and lots of people asked me for it so I'm going to remake it";
+  const introText = "I used to have a list of things, mainly tools I reccomended to early stage companies or patch teams, usually to speed up a process. In some cases it's as an effective low cost alternatives to something they already used. I lost that file and lots of people asked me for it so I'm going to remake it";
 
   return (
     <FilterableList items={resources} introText={introText} />
